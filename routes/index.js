1b7824f718f91ae0cdec7432e5e7ca174672badc
LoordhuJeyakumar <loordhujeyakumar@gmail.com>
2024-01-10 06:41:54 +0530

    homepage content changed

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const studentRouter = require("./student.js");
 
 appRouter.get("/", (req, res) =>
   res.status(200).send(`<h1>Mentor and Student Assigning with Database
-  </h1><b style="color:white; background-color:green; padding:5">Application Health is Good</b>`)
+  </h1><b style="color:white; background-color:green; padding:5">Connected to MongoDB Application Health is Good</b>`)
 );
 appRouter.use("/api/v1/mentors", mentorRouter);
 appRouter.use("/api/v1/students", studentRouter);
